505a143f50660aeee72dfbb8c0a56d1b322da289
ludovicoc <ludovicoc@allor.io>
2024-02-29 16:52:58 +0100

    added social up top

--- src/containers/TopbarContainer/Topbar/TopbarDesktop/TopbarDesktop.js
+++ src/containers/TopbarContainer/Topbar/TopbarDesktop/TopbarDesktop.js
@@ -224,6 +224,7 @@ const TopbarDesktop = props => {
             {search}
           </div>
           <div className={css.rightContent}>
+            <SocialBar />
             {userRole === 'provider' && (
               <NamedLink className={css.createListingLink} name="NewListingPage">
                 <span className={css.createListing}>
