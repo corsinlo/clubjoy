b0a81a411ba18a28d741ee5a66aa70829f76e84e
ludovicoc <ludovicoc@allor.io>
2024-03-01 16:59:33 +0100

    landing title fix

--- src/components/LandingSearchBarContainer/LandingSearchBarContainer.js
+++ src/components/LandingSearchBarContainer/LandingSearchBarContainer.js
@@ -7,7 +7,7 @@ const LandingSearchBarContainer = ({ onSearchSubmit }) => {
   return (
     <div className={css.landingBarContainer} style={{ backgroundImage: `url(${landingCover})` }}>
       <div className={css.introText}>Di creativo ti rimane solo il parcheggio?</div>
-      <div className={css.introText2}>Scopri il tuo nuovo hobby con Club joy</div>
+      <div className={css.introText2}>Scopri il tuo nuovo hobby preferito con Club joy</div>
       <LandingSearchBarForm onSearchSubmit={onSearchSubmit} />
     </div>
   );
