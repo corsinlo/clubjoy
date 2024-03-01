aa9538e86d8ee73e74aec02f4527501120550d22
ludovicoc <ludovicoc@allor.io>
2024-03-01 18:00:49 +0100

    fix search bar mobile bars

--- src/components/LandingSearchBarContainer/LandingSearchBarContainer.js
+++ src/components/LandingSearchBarContainer/LandingSearchBarContainer.js
@@ -5,9 +5,12 @@ import LandingSearchBarForm from './LandingSearchBarForm';
 
 const LandingSearchBarContainer = ({ onSearchSubmit }) => {
   return (
-    <div className={css.landingBarContainer} style={{ backgroundImage: `url(${landingCover})` }}>
+    <div
+      className={css.landingBarContainer}
+      style={{ backgroundImage: `url(${landingCover})`, backgroundSize: 'cover' }}
+    >
       <div className={css.introText}>Di creativo ti rimane solo il parcheggio?</div>
-      <div className={css.introText2}>Scopri il tuo nuovo hobby preferito con Club joy</div>
+      <div className={css.introText2}>Scopri il tuo nuovo hobby preferito con Club Joy</div>
       <LandingSearchBarForm onSearchSubmit={onSearchSubmit} />
     </div>
   );
