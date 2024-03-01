9fb562deb8bfb381f7bbff41e4dd728bea731216
ludovicoc <ludovicoc@allor.io>
2024-03-01 23:34:14 +0100

    landingcover fix

--- src/components/LandingSearchBarContainer/LandingSearchBarContainer.js
+++ src/components/LandingSearchBarContainer/LandingSearchBarContainer.js
@@ -7,7 +7,12 @@ const LandingSearchBarContainer = ({ onSearchSubmit }) => {
   return (
     <div
       className={css.landingBarContainer}
-      style={{ backgroundImage: `url(${landingCover})`, backgroundSize: 'cover' }}
+      style={{
+        backgroundImage: `url(${landingCover})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        height: '700px',
+      }}
     >
       <div className={css.introText}>Di creativo ti rimane solo il parcheggio?</div>
       <div className={css.introText2}>Scopri il tuo nuovo hobby preferito con Club Joy</div>
