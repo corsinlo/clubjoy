6b580569e368c213dd5b11bbc558ea2d467209b9
ludovicoc <ludovicoc@allor.io>
2024-03-24 00:56:34 +0100

    Counter

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import css from './Counter.module.css';
+import { createClient } from '@supabase/supabase-js';
+import { useIntl } from 'react-intl';
+const supabaseUrl = 'https://tivsrbykzsmbrkmqqwwd.supabase.co';
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const intl = useIntl();
+
+  useEffect(() => {
+    // Function to fetch the initial counter value from the database
+    const initializeCounter = async () => {
+      const { data, error } = await supabase
+        .from('counter')
+        .select('value')
+        .eq('id', 1) // Assuming you want to fetch the row where id=1
+        .single();
+
+      if (error) {
+        console.error('Error fetching counter', error);
+      } else {
+        setCounter(data.value);
+      }
+    };
+
+    initializeCounter();
+  }, []);
+
+  return (
+    <div className={css.formContainer}>
+      <h3 style={{ color: 'white' }}>
+        <span className={css.counterValue}>
+          {String(counter)
+            .split('')
+            .map((digit, index) => (
+              <span key={index} className={css.digitBox}>
+                {digit}
+              </span>
+            ))}
+        </span>
+      </h3>
+      <div className={css.counterTitle}>{intl.formatMessage({ id: 'Counter.title' })}</div>
+    </div>
+  );
+};
+
+export default Counter;
