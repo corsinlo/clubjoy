5621f3a7d678d0258e1bb4cb67659b4c9a71fc2a
lcorsini <ludovico.corsini@certilogo.com>
2024-06-12 12:55:20 +0200

    fixed email

--- server/api/brevo/notifyinvoice.js
+++ server/api/brevo/notifyinvoice.js
@@ -16,64 +16,20 @@ apiKey.apiKey = process.env.BREVO_API_KEY;
 let sendSmtpEmail = new SibApiV3.SendSmtpEmail();
 
 module.exports = async (req, res) => {
-    console.log('herre', req.body);
-    /*
+  const formattedDate = (dateString => new Date(dateString).toLocaleDateString('it-IT', { timeZone: 'UTC' }).replace(/\//g, '-'))(req.body.customerObj.startdate);  
   try {
-    // Query Supabase to find the record by bookingid
-
-
-    const { data, error } = await supabase
-      .from('bookings')
-      .select('*')
-      .eq('bookingid', req.body.customerObj.bookingid);
-
-    if (error) {
-      console.error('Error fetching booking:', error);
-      return res.status(500).json({ error: 'Error fetching booking' });
-    }
-
-    if (data.length === 0) {
-      return res.status(404).json({ error: 'Booking not found' });
-    }
-
-    const bookingRecord = data[0];
-    console.log('Booking record:', bookingRecord);
-
-    if (bookingRecord) {
       sendSmtpEmail.sender = { name: 'Club Joy Team', email: 'noreply@clubjoy.it' };
-      sendSmtpEmail.to = [{ email: 'corsini.ludovico@gmail.com', name: bookingRecord.providername }]; //bookingRecord.providerEmail
-      sendSmtpEmail.templateId = 28;
+      sendSmtpEmail.to = [{ email: 'corsini.ludovico@gmail.com', name: 'test' }]; //bookingRecord.providerEmail
+      sendSmtpEmail.templateId = 31;
       sendSmtpEmail.params = {
-        providerName: bookingRecord.providername,
-        userName: bookingRecord.name,
-        startDate: bookingRecord.startdate,
+        providerName: req.body.customerObj.providername,
+        username: req.body.customerObj.name,
+        startDate: formattedDate,
       };
-
-      try {
         const emailResponse = await brevoClient.sendTransacEmail(sendSmtpEmail);
         console.log('Email sent successfully to provider', emailResponse);
-        try {
-          sendSmtpEmail.sender = { name: 'Club Joy Team', email: 'noreply@clubjoy.it' };
-          sendSmtpEmail.to = [{ email: 'corsini.ludovico@gmail.com', name: bookingRecord.providername }]; //bookingRecord.providerEmail
-          sendSmtpEmail.templateId = 29;
-          sendSmtpEmail.params = {
-            providerName: bookingRecord.providername,
-            userName: bookingRecord.name,
-            startDate: bookingRecord.startdate,
-          };
-          const emailResponse = await brevoClient.sendTransacEmail(sendSmtpEmail);
-          res.json({ message: 'Email sent successfully to customer', data: emailResponse });
-        } catch {
-          console.error('Failed to send customer email', emailError);
-        }
       } catch (emailError) {
         console.error('Error sending email:', emailError);
         res.status(500).json({ error: 'Failed to send provider email', emailError });
       }
-    }
-  } catch (err) {
-    console.error('Error:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-  */
 };
